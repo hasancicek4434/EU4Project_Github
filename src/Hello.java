4276e3a9cbb9f4ed241013aa2ad5a39d522af0e2
hasancicek4434 <76873386+hasancicek4434@users.noreply.github.com>
2021-02-01 23:55:27 +0300

    after sharing commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
 // hello world
         // we are good at github
+        // it is after sharing
+        // to practice push
     }
 }
